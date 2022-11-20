725a82087e2929ad68ce2462d5a6938ac121a394
Alessandro Serafini <aserafini@veepee.com>
2022-11-20 12:05:54 +0100

    ci(ci): bypassing Jekyll on GitHub Pages
    Added .nojekyll file in the folder that contains the _next folder

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/alessandroserafini-web/alessandroserafini-web.github.io/gh-pages/"
-    : "",
 };
 
 module.exports = nextConfig;
